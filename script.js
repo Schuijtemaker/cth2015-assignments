e74cc74ac1699e073419a5c1fc92280a3a8ddd1f
Schuijtemaker <lschuijtemaker@hotmail.com>
2015-11-18 11:33:01 +0000

    Add three scripts to the repository

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 console.log("Welcome!");
 var image = document.getElementById("image");
 var imageNext = 0;
-var imageChange = ["http://yatron.kucdweb.com/letterpng/aleph.png", "http://www.i2symbol.com/images/cool-letters/syriac/armenian_capital_letter_eh_u0537_icon_256x256.png", "https://panjewish.files.wordpress.com/2015/09/cuneiform_sumer_dingir.png?w=300", "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Devanagari_aa.svg/480px-Devanagari_aa.svg.png", "http://modrodnovery.com/wp-content/uploads/2015/04/600px-Glagolitic_yu.svg_.png", "https://qph.is.quoracdn.net/main-qimg-8781ad604a303ae4b9917e6b0c2d5eb1?convert_to_webp=true", "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Li_%28%E6%9D%8E%29.svg/1024px-Li_%28%E6%9D%8E%29.svg.png"];
+var imageChange = [];
 var turn = Math.floor(Math.random() * 4) * 90;
 var zoom = 200;
-image.style.transform = "rotate(" + turn + "deg)";
 image.style.width = zoom + "px";
 
 //This is the rotate function.
@@ -17,7 +16,7 @@ var rotateImage = function (angle) {
     }
 };
 
-//This is the check image/letter function.
+//This is the check image function.
 var checkImage = function() {
     console.log("Click registered (check)");
     if(turn === 0) {
@@ -36,18 +35,49 @@ var scaleImage = function(scale) {
     if(zoom <= 50) {
         zoom = 50;
     }
-    else if(zoom >= 350) {
-        zoom = 350;
+    else if(zoom >= 300) {
+        zoom = 300;
     }
     image.style.width = zoom + "px";
 };
 
-//This is the next image/letter function.
+//This is the next and previous image function.
 var getImage = function(imageNextPlus) {
+    image.src = "http://www.eurobasket.com/images/loader.gif";
     console.log("Click registered (next)");
     turn = Math.floor(Math.random() * 4) * 90;
     image.style.transform = "rotate(" + turn + "deg)";
     imageNext += imageNextPlus;
     if(imageNext >= imageChange.length) {imageNext = 0;}
+    else if(imageNext < 0) {imageNext = imageChange.length - 1;}
     image.src = imageChange[imageNext];
 };
+
+//This is were I request the Medieval manuscript pictures from Europeana. 
+//I've chosen this kind of images because of three reasons. 
+//First and foremost: my historical interests. 
+//Second: the fact that the ancient writing style makes it sometimes hard to recognize which side is up.
+//Third: I wanted something different than modern art, because most people will probably choose that.
+var query = "europeana_collectionName: 2021003*";
+var totalImages = 30;
+var url = "http://www.europeana.eu/api/v2/search.json?wskey=NHCsNVJXN&query=" + query + "&start=1&rows=" + totalImages + "&profile=standard";
+var myRequest = new XMLHttpRequest();
+myRequest.open("GET", url);
+myRequest.send();
+
+//This function places the images from Europeana into the arrey of the variable 'imageChange'.
+myRequest.onreadystatechange = function() {
+    console.log("readyState = " + myRequest.readyState);
+    if (myRequest.readyState === 4) {
+        console.log(myRequest.responseText);
+        var parse = JSON.parse(myRequest.responseText);
+        var itemsCount = parse.itemsCount;
+        for (var i = 0; i < itemsCount; i++) {
+            if (typeof parse.items[i].edmPreview != "undefined") {
+               imageChange.push(parse.items[i].edmPreview[0]);
+            }
+        image.src = imageChange[0];
+        }
+    }
+};
+
